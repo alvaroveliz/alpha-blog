a97dcfaaf27211d0163e1a4243ed09e9e9774fdd
Álvaro Véliz <alvaro.veliz@checkr.com>
2022-05-12 10:57:38 -0400

    Adding create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,13 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article 
+  end
 end
